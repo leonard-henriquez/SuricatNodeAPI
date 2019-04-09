54e4f501d6fa1d4509a31e01d6e7753ea05ab6d0
Leonard-Henriquez <leonard.henriquez@edhec.com>
2019-04-09 04:38:26 +0200

    Added tasks

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,8 +1,8 @@
-const morgan = require('morgan');
-const methodOverride = require('method-override');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const compression = require('compression');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cors = require('cors');
 const logger = require('./logger');
 const config = require('./');
 
@@ -10,20 +10,20 @@ const middlewares = (app) => {
   // Disable superfluous header
   app.disable('x-powered-by');
 
+  // Add compression
+  app.use(compression());
+
   // Add logger
   app.use(morgan(config.logFormat, { stream: logger.stream }));
-
-  // Add method overrider
-  app.use(methodOverride());
 
   // Add json parser
   app.use(bodyParser.json());
 
+  // Add method overrider
+  app.use(methodOverride());
+
   // Add CORS headers
   app.use(cors({ origin: '*' }));
-
-  // Add compression
-  app.use(compression());
 
   return app;
 };
