c24dd16d12f3d250da0c6b2b65d72e9f203cf6ca
Leonard-Henriquez <leonard.henriquez@edhec.com>
2019-04-09 03:51:13 +0200

    Refactor file structure

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,20 +1,29 @@
-const cors = require('cors');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const compression = require('compression');
 const logger = require('./logger');
+const config = require('./');
 
-const middlewares = (app, { config }) => {
+const middlewares = (app) => {
   // Disable superfluous header
   app.disable('x-powered-by');
-
-  // Add CORS headers
-  app.use(cors({ origin: '*' }));
 
   // Add logger
   app.use(morgan(config.logFormat, { stream: logger.stream }));
 
+  // Add method overrider
+  app.use(methodOverride());
+
   // Add json parser
   app.use(bodyParser.json());
+
+  // Add CORS headers
+  app.use(cors({ origin: '*' }));
+
+  // Add compression
+  app.use(compression());
 
   return app;
 };
